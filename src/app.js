3058a88d6831072efc7c36ae2ea33d8749f6b79f
LeonardoCalonico <dcalonico2@gmail.com>
2026-01-12 23:31:52 +0000

    Generador de Cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let icons = ["♦", "♥", "♠", "♣"];
+
+  let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+  
+  let card = document.querySelector("#center");
+  let domIconUp = document.querySelector("#swordUp");
+  let domIconDown = document.querySelector("#swordDown");
+
+  if(randomIcon === "♥" || randomIcon === "♦") {
+    domIconUp.style.color = "red";
+    domIconDown.style.color = "red";
+
+  }
+
+  card.innerText = randomNum;
+  domIconUp.innerText = randomIcon;
+  domIconDown.innerText = randomIcon;
+
+}
